32d8fbb5ff5f9608f766946b760754ce31e9addf
Richard Luo <richard.luo@hivery.com>
2020-04-05 01:49:26 +1100

    Adding prettier to the development and eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
-module.exports = {
-  semi: true,
-  singleQuote: false,
-  printWidth: 100,
-  tabWidth: 2,
-}
+module.exports =  {
+  semi:  true,
+  trailingComma:  'all',
+  singleQuote:  false,
+  printWidth:  100,
+  tabWidth:  2,
+};
