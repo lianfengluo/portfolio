5c8d72e5715f7e8e08aa67fcc845ea6b7b5d2c03
Richard Luo <richard.luo@hivery.com>
2020-04-05 01:51:43 +1100

    Adding blank about and projects pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles/About.module.scss";
+
+const About: React.FC<{}> = () => {
+  return <div className={styles["about"]}></div>;
+};
+
+export default About;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,24 +1,8 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import "./styles/Projects.scss";
+import styles from "./styles/Projects.module.scss";
 
-const Home: React.FC<{}> = () => {
-    return (
-      <div className="home">
-        <div className="intro">Hi,</div>
-        <div className="intro">I'm <span className="name">Richard</span></div>
-        <div className="bio">
-          Web developer (Frontend focus)
-        </div>
-        <div className="intro">Javascript / React / HTML / CSS / Python / REST-API</div>
-        <div className="icons">
-          <a href="https://github.com/lianfengluo/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-          <a href="https://www.linkedin.com/in/richard-luo-849a87103/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-          <a href="mailto:lianfengluo28@gmail.com" target="_top"><FontAwesomeIcon icon={faEnvelope} /></a>
-        </div>
-      </div>
-    )
-}
+const Projects: React.FC<{}> = () => {
+  return <div className={styles["projects"]}></div>;
+};
 
-export default Home;
+export default Projects;
