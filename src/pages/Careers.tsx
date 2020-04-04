c5f41d68d5f4be613739306aaee752144bdd6b04
Richard Luo <richard.luo@hivery.com>
2020-04-05 01:51:26 +1100

    Adding Home and Careers page

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,72 +1,117 @@
-import React from "react";
-import "./styles/Careers.scss";
+import React, { useRef } from "react";
+import styles from "./styles/Careers.module.scss";
 
 const Careers: React.FC<{}> = () => {
-    return (
-      <div className="careers">
-        <div className="info">
-          <div className="hivery">
-            <div className="company">Hivery</div>
-            <div className="job-title">Full Stack Software Engineer</div>
-            <div className="description">
-              <p>
-                I am working with React Hook, Redux and Typescript in the fronend 
-                and some backend development related to Django REST framework.
-              </p>
-              <div className="date">
-                FEB 2020 – PRESENT
-              </div>
-            </div>
-          </div>
-
-          <div className="UNSW">
-            <div className="company">UNSW</div>
-            <div className="job-title">Course Developer</div>
-            <div className="description">
-              <p>
-                I worked with professor Eric Martin to design the structure of a UNSW online data science course (ZZEN9021).
-              </p>
-              <div className="date">
-                FEB 2019 – JUN 2019
-              </div>
-            </div>
-          </div>
-
-          <div className="UNSW">
-            <div className="company">UNSW</div>
-            <div className="job-title">Student Tutor</div>
-            <div className="description">
-              <p>
-                I helped my students improve their Python skills by programming code practices and solving quizze and as- signments 
-                together during the consultation.<br/>
-                I tried different strategies to help my students come up with efficient solutions under strict memory limit 
-                requirements in a course about Data Compression and Web Search.
-              </p>
-              <div className="date">
-                JUL 2018 – AUG 2019
-              </div>
-            </div>
-          </div>
-
-          <div className="NetEase">
-            <div className="company">NetEase</div>
-            <div className="job-title">Software Engineer</div>
-            <div className="description">
-              <p>
-                I worked on both frontend and backend to build the OA system for the company. <br/>
-                I used Django REST framework (DRF) for the backend development and provided API for frontend.
-              </p>
-              <div className="date">
-                JUL 2017 - OCT 2017
-              </div>
-            </div>
+  const hiveryRef = useRef<HTMLDivElement | null>();
+  const UNSWRef = useRef<HTMLDivElement | null>();
+  const NetEaseRef = useRef<HTMLDivElement | null>();
+  return (
+    <div className={styles["careers"]}>
+      <div className={styles["info"]}>
+        <div
+          className={styles["hivery"]}
+          onMouseOver={() => {
+            hiveryRef.current?.classList.add(styles["active"]);
+          }}
+          onMouseOut={() => {
+            hiveryRef.current?.classList.remove(styles["active"]);
+          }}
+        >
+          <div className={styles["company"]}>Hivery</div>
+          <div className={styles["job-title"]}>Full Stack Software Engineer</div>
+          <div className={styles["description"]}>
+            <p>
+              I am working with React Hook, Redux and Typescript in the fronend and some backend
+              development related to Django REST framework.
+            </p>
+            <div className={styles["date"]}>FEB 2020 – PRESENT</div>
           </div>
         </div>
 
-        <div className="career-logos">
+        <div
+          className={styles["UNSW"]}
+          onMouseOver={() => {
+            UNSWRef.current?.classList.add(styles["active"]);
+          }}
+          onMouseOut={() => {
+            UNSWRef.current?.classList.remove(styles["active"]);
+          }}
+        >
+          <div className={styles["company"]}>UNSW</div>
+          <div className={styles["job-title"]}>Course Developer</div>
+          <div className={styles["description"]}>
+            <p>
+              I worked with professor Eric Martin to design the structure of a UNSW online data
+              science course (ZZEN9021).
+            </p>
+            <div className={styles["date"]}>FEB 2019 – JUN 2019</div>
+          </div>
+        </div>
+
+        <div
+          className={styles["UNSW"]}
+          onMouseOver={() => {
+            UNSWRef.current?.classList.add(styles["active"]);
+          }}
+          onMouseOut={() => {
+            UNSWRef.current?.classList.remove(styles["active"]);
+          }}
+        >
+          <div className={styles["company"]}>UNSW</div>
+          <div className={styles["job-title"]}>Student Tutor</div>
+          <div className={styles["description"]}>
+            <p>
+              I helped my students improve their Python skills by programming code practices and
+              solving quizze and as- signments together during the consultation.
+              <br />I tried different strategies to help my students come up with efficient
+              solutions under strict memory limit requirements in a course about Data Compression
+              and Web Search.
+            </p>
+            <div className={styles["date"]}>JUL 2018 – AUG 2019</div>
+          </div>
+        </div>
+
+        <div
+          className={styles["NetEase"]}
+          onMouseOver={() => {
+            NetEaseRef.current?.classList.add(styles["active"]);
+          }}
+          onMouseOut={() => {
+            NetEaseRef.current?.classList.remove(styles["active"]);
+          }}
+        >
+          <div className={styles["company"]}>NetEase</div>
+          <div className={styles["job-title"]}>Software Engineer</div>
+          <div className={styles["description"]}>
+            <p>
+              I worked on both frontend and backend to build the OA system for the company. <br />I
+              used Django REST framework (DRF) for the backend development and provided API for
+              frontend.
+            </p>
+            <div className={styles["date"]}>JUL 2017 - OCT 2017</div>
+          </div>
         </div>
       </div>
-    )
-}
+
+      <div className={styles["career-logos"]}>
+        <div ref={(ref) => (hiveryRef.current = ref)}>
+          <img src="https://s3.amazonaws.com/hivery-images/icon_hivery-logo.svg" alt="Hivery" />
+        </div>
+        <div ref={(ref) => (UNSWRef.current = ref)}>
+          <img
+            src="https://seekvectorlogo.com/wp-content/uploads/2018/01/unsw-sydney-vector-logo.png"
+            alt="UNSW"
+          />
+        </div>
+        <div ref={(ref) => (NetEaseRef.current = ref)}>
+          <img
+            src="https://netease-na.com/wp-content/uploads/2015/08/netease-logo.jpg"
+            alt="NetEase"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Careers;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import "./styles/Home.scss";
+import styles from "./styles/Home.module.scss";
 
 const Home: React.FC<{}> = () => {
-    return (
-      <div className="home">
-        <div className="intro">Hi,</div>
-        <div className="intro">I'm <span className="name">Richard</span></div>
-        <div className="bio">
-          Web developer (Frontend focus)
-        </div>
-        <div className="intro">Javascript / React / HTML / CSS / Python / REST-API</div>
-        <div className="icons">
-          <a href="https://github.com/lianfengluo/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-          <a href="https://www.linkedin.com/in/richard-luo-849a87103/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-          <a href="mailto:lianfengluo28@gmail.com" target="_top"><FontAwesomeIcon icon={faEnvelope} /></a>
-        </div>
+  return (
+    <div className={styles["home"]}>
+      <div className={styles["intro"]}>Hi,</div>
+      <div className={styles["intro"]}>
+        I'm <span className={styles["name"]}>Richard</span>
       </div>
-    )
-}
+      <div className={styles["bio"]}>Web developer (Frontend focus)</div>
+      <div className={styles["intro"]}>Javascript / React / HTML / CSS / Python / REST-API</div>
+      <div className={styles["icons"]}>
+        <a href="https://github.com/lianfengluo/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={["fab", "github"]} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/richard-luo-849a87103/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={["fab", "linkedin"]} />
+        </a>
+        <a href="mailto:lianfengluo28@gmail.com" target="_top">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
