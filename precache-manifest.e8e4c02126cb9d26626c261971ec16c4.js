f50807319bd450afc11cefd1ca33a49aef8cd841
Richard Luo <richard.luo@hivery.com>
2020-04-05 18:42:29 +1000

    Updates

---
+++ precache-manifest.e8e4c02126cb9d26626c261971ec16c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a480a55e0942bd459309106d19b295e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8d427cac5dd77ebf1614",
+    "url": "/portfolio/static/css/main.2124bcc0.chunk.css"
+  },
+  {
+    "revision": "0b7e74842cd4c33e5377",
+    "url": "/portfolio/static/js/2.b533b738.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b533b738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d427cac5dd77ebf1614",
+    "url": "/portfolio/static/js/main.f3b12ce1.chunk.js"
+  },
+  {
+    "revision": "5e4cd4c6f14baf3f4e46",
+    "url": "/portfolio/static/js/runtime-main.86f1c7e8.js"
+  }
+]);
