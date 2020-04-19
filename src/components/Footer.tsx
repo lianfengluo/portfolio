c0b48bebebb801d1cb7536b83fb626c6068373ad
Richard Luo <richard.luo@hivery.com>
2020-04-19 23:59:05 +1000

    Fix header and footer animation problem

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import styles from "./styles/Footer.module.scss";
 
-const Footer: React.FC<{}> = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.author}>
-        Author: <span>Richard Luo</span>
-      </div>
-    </footer>
-  );
-};
+const Footer: React.FC<{}> = () => 
+  <footer className={styles.footer}>
+    <div className={styles.author}>
+      Author: <span>Richard Luo</span>
+    </div>
+  </footer>
 
 export default Footer;
